3a2800a705f185c73d5e95bf2931f03105c79cce
KatharinaWuerth <K.Wuerth@gmx.net>
2019-06-11 18:16:37 +0200

    Scale img and add media queries

--- src/filterPage/OptionButton.js
+++ src/filterPage/OptionButton.js
@@ -4,9 +4,12 @@ const OptionButton = styled.button`
   padding: 10px;
   border-radius: 7px;
   background: ${props => (props.active ? '#CBE1C2' : 'white')};
-  font-size: medium;
+  font-size: 0.85rem;
   box-shadow: 2px 2px 9px -1px darkgray;
   outline: none !important;
+  @media screen and (min-width: 600px) {
+    font-size: 1.7rem;
+  }
 `
 
 export default OptionButton
--- src/plantPage/PlantTagList.js
+++ src/plantPage/PlantTagList.js
@@ -7,12 +7,15 @@ const StyledTagList = styled.section`
 `
 
 const StyledTag = styled.span`
-  border: solid black 0.7px;
+  border: solid #367860 0.7px;
   border-radius: 7px;
   padding: 3px;
   margin: 3px;
   font-size: 0.85rem;
   background: ${props => (props.active ? '#CBE1C2' : 'white')};
+  @media screen and (min-width: 600px) {
+    font-size: 1.7rem;
+  }
 `
 
 export default function TagList({ tags, getOptionLabel, selection }) {
